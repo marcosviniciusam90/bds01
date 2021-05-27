2a4cf93ed14ac9589922475951753b3e43a21bf7
Marcos <marcos.mendonca@matera.com>
2021-05-27 17:23:23 -0300

    Adicionando @Transactional no método findAll do Service

--- src/main/java/com/devsuperior/bds01/services/DepartmentService.java
+++ src/main/java/com/devsuperior/bds01/services/DepartmentService.java
@@ -6,6 +6,7 @@ import com.devsuperior.bds01.repositories.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,6 +17,7 @@ public class DepartmentService {
     @Autowired
     private DepartmentRepository departmentRepository;
 
+    @Transactional
     public List<DepartmentDTO> findAll() {
         List<Department> list = departmentRepository.findAll(Sort.by("name"));
         return list.stream().map(DepartmentDTO::new).collect(Collectors.toList());
